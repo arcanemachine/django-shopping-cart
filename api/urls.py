db706f0712ae7bd36089cf684057bc2c03fee21f
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-19 17:21:37 -0700

    Re-organize API views/naming scheme

--- api/urls.py
+++ api/urls.py
@@ -7,31 +7,26 @@ urlpatterns = [
     path('',
          views.api_root,
          name='api_root'),
+
+    # list
     path('stores/',
          views.StoreList.as_view(),
          name='store_list'),
-    path('stores/<int:store_pk>/',
-         views.StoreDetail.as_view(),
-         name='store_detail'),
-    path('stores/<int:store_pk>/categories/',
-         views.CategoryList.as_view(),
-         name='category_list'),
-    path('stores/<int:store_pk>/categories/<int:category_pk>/',
-         views.StoreCategoryDetail.as_view(),
-         name='store_category_detail'),
-    path('stores/<int:store_pk>/categories/<int:category_pk>/items/',
-         views.ItemList.as_view(),
-         name='item_list'),
-    path('stores/<int:store_pk>/categories/<int:category_pk>/items/'
-         '<int:item_pk>/',
-         views.StoreItemDetail.as_view(),
-         name='item_detail'),
-
-    # short urls
     path('categories/<int:category_pk>/',
          views.CategoryDetail.as_view(),
          name='category_detail'),
     path('items/<int:item_pk>/',
          views.ItemDetail.as_view(),
          name='item_detail'),
+
+    # detail
+    path('stores/<int:store_pk>/',
+         views.StoreDetail.as_view(),
+         name='store_detail'),
+    path('stores/<int:store_pk>/categories/',
+         views.StoreCategoryList.as_view(),
+         name='store_category_list'),
+    path('categories/<int:category_pk>/items/',
+         views.CategoryItemList.as_view(),
+         name='item_list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from .permissions import HasStorePermissionsOrReadOnly
@@ -11,23 +10,14 @@ def api_root(request):
     return JsonResponse({'message': 'hello world!'})
 
 
+# list
+
 class StoreList(generics.ListCreateAPIView):
-    # permission_classes = [IsAuthenticated]
     queryset = Store.objects.all()
     serializer_class = serializers.StoreSerializer
 
 
-class StoreDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [HasStorePermissionsOrReadOnly]
-    serializer_class = serializers.StoreSerializer
-    lookup_url_kwarg = 'store_pk'
-
-    def get_queryset(self):
-        return Store.objects.filter(pk=self.kwargs['store_pk'])
-
-
-class CategoryList(generics.ListCreateAPIView):
-    # permission_classes = [IsAuthenticated]
+class StoreCategoryList(generics.ListCreateAPIView):
     serializer_class = serializers.CategorySerializer
 
     def check_permissions(self, request):
@@ -44,22 +34,7 @@ class CategoryList(generics.ListCreateAPIView):
         return Category.objects.filter(store=self.kwargs['store_pk'])
 
 
-class StoreCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [HasStorePermissionsOrReadOnly]
-    serializer_class = serializers.CategorySerializer
-    lookup_url_kwarg = 'category_pk'
-
-    def check_permissions(self, request):
-        super().check_permissions(request)
-        obj = Store.objects.get(pk=self.kwargs['store_pk'])
-        super().check_object_permissions(request, obj)
-
-    def get_queryset(self):
-        return Category.objects.filter(pk=self.kwargs['store_pk'])
-
-
-class ItemList(generics.ListCreateAPIView):
-    # permission_classes = [IsAuthenticated]
+class CategoryItemList(generics.ListCreateAPIView):
     serializer_class = serializers.ItemSerializer
 
     def check_permissions(self, request):
@@ -76,21 +51,16 @@ class ItemList(generics.ListCreateAPIView):
         return Item.objects.filter(category=self.kwargs['category_pk'])
 
 
-class StoreItemDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [HasStorePermissionsOrReadOnly]
-    serializer_class = serializers.ItemSerializer
-    lookup_url_kwarg = 'item_pk'
+# detail
 
-    def check_permissions(self, request):
-        super().check_permissions(request)
-        obj = Store.objects.get(pk=self.kwargs['store_pk'])
-        super().check_object_permissions(request, obj)
+class StoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [HasStorePermissionsOrReadOnly]
+    serializer_class = serializers.StoreSerializer
+    lookup_url_kwarg = 'store_pk'
 
     def get_queryset(self):
-        return Item.objects.filter(pk=self.kwargs['store_pk'])
+        return Store.objects.filter(pk=self.kwargs['store_pk'])
 
-
-# short urls
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [HasStorePermissionsOrReadOnly]
