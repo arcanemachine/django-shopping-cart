44960483a9304eafd9732306d8dae7f563d91da2
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-19 17:44:31 -0700

    Fix improper ordering

--- api/urls.py
+++ api/urls.py
@@ -12,21 +12,21 @@ urlpatterns = [
     path('stores/',
          views.StoreList.as_view(),
          name='store_list'),
-    path('categories/<int:category_pk>/',
-         views.CategoryDetail.as_view(),
-         name='category_detail'),
-    path('items/<int:item_pk>/',
-         views.ItemDetail.as_view(),
-         name='item_detail'),
-
-    # detail
-    path('stores/<int:store_pk>/',
-         views.StoreDetail.as_view(),
-         name='store_detail'),
     path('stores/<int:store_pk>/categories/',
          views.StoreCategoryList.as_view(),
          name='store_category_list'),
     path('categories/<int:category_pk>/items/',
          views.CategoryItemList.as_view(),
          name='item_list'),
+
+    # detail
+    path('stores/<int:store_pk>/',
+         views.StoreDetail.as_view(),
+         name='store_detail'),
+    path('categories/<int:category_pk>/',
+         views.CategoryDetail.as_view(),
+         name='category_detail'),
+    path('items/<int:item_pk>/',
+         views.ItemDetail.as_view(),
+         name='item_detail'),
 ]
