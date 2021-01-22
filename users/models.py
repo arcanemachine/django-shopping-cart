3ba7029da424e5e3a328c364412cf17b303a6d63
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-21 23:10:51 -0700

    Create user Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
+    cart = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f"user '{self.user.username}'"
+
+    def get_absolute_url(self):
+        return self.user.get_absolute_url()
