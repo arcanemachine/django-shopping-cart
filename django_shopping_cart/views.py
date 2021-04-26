f6dd7742974093105e5924c76720567bdb8ef6e8
Nicholas Moen <arcanemachine@tutanota.com>
2021-04-26 15:20:07 -0600

    Remove media dir, remove experimental views

--- django_shopping_cart/views.py
+++ django_shopping_cart/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect  # , JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.clickjacking import xframe_options_exempt
+# from django.views.decorators.clickjacking import xframe_options_exempt
 
 from django_shopping_cart import server_config
 
 FRONTEND_SERVER_LOCATION = server_config.FRONTEND_SERVER_LOCATION
 FRONTEND_LOGIN_URL = server_config.FRONTEND_LOGIN_URL
+
 
 def project_root(request):
     return render(request, 'project_root.html')
@@ -16,15 +17,12 @@ def hello_cookie(request):
     response = \
         HttpResponseRedirect(FRONTEND_SERVER_LOCATION + FRONTEND_LOGIN_URL)
     response.set_cookie(key="hello", value=123, httponly=True)
-
     return response
 
 
 @ensure_csrf_cookie
 def get_csrftoken(request):
-
     response = \
         HttpResponseRedirect(FRONTEND_SERVER_LOCATION + FRONTEND_LOGIN_URL)
     response.set_cookie(key="hello", value=123)
-
     return response
