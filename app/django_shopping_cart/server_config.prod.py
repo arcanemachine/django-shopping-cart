b039363ef40a137ea8fdb8dbc290a139a13b9fec
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-21 02:44:10 -0600

    Do not use trailing slash for CORS allowed origins

--- app/django_shopping_cart/server_config.prod.py
+++ app/django_shopping_cart/server_config.prod.py
@@ -8,5 +8,5 @@ SERVER_LOCATION = 'https://django-shopping-cart.nicholasmoen.com/'
 
 FRONTEND_SERVER_LOCATION = 'https://vue-shopping-cart2.surge.sh/'
 
-CORS_ALLOWED_ORIGINS = [FRONTEND_SERVER_LOCATION]
+CORS_ALLOWED_ORIGINS = ['https://vue-shopping-cart2.surge.sh']
 CORS_ALLOW_ALL_ORIGINS = False
