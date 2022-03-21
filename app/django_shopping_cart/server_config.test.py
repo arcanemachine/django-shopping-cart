4977bb78bf269e5be759e7dbf84d866374d6d563
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-21 01:40:41 -0600

    Use shorter test domain name

--- app/django_shopping_cart/server_config.test.py
+++ app/django_shopping_cart/server_config.test.py
@@ -4,15 +4,15 @@ from pathlib import Path
 BASE_DIR = str(Path(__file__).resolve().parent.parent)
 DEBUG = False
 ALLOWED_HOSTS =\
-    ['django-shopping-cart.nicholasmoen.com.test.moendigitalservices.com']
+    ['django-shopping-cart.test.moendigitalservices.com']
 
 PROJECT_NAME = "Django Shopping Cart"
 SERVER_NAME = 'prod'
 SERVER_LOCATION =\
-    'https://django-shopping-cart.nicholasmoen.com.test.moendigitalservices.com/'  # noqa: E501
+    'https://django-shopping-cart.test.moendigitalservices.com/'  # noqa: E501
 
 FRONTEND_SERVER_LOCATION = 'https://vue-shopping-cart2.surge.sh/'
 
 CORS_ALLOWED_ORIGINS = \
-    ['https://django-shopping-cart.nicholasmoen.com.test.moendigitalservices.com']  # noqa: E501
+    ['https://django-shopping-cart.test.moendigitalservices.com']  # noqa: E501
 CORS_ALLOW_ALL_ORIGINS = False
