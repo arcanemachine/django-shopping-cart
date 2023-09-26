76c08e1ae62473d4a9b63ab286de628f67cd755c
Nicholas Moen <arcanemachine@tutanota.com>
2023-09-26 17:30:38 -0600

    chore: remove trailing slash from CORS_ALLOWED_ORIGINS entry

--- app/django_shopping_cart/server_config.prod.py
+++ app/django_shopping_cart/server_config.prod.py
@@ -8,5 +8,5 @@ SERVER_LOCATION = 'https://django-shopping-cart.nicholasmoen.com/'
 
 FRONTEND_SERVER_LOCATION = 'https://vue-shopping-cart.nicholasmoen.com/'
 
-CORS_ALLOWED_ORIGINS = ['https://vue-shopping-cart.nicholasmoen.com/']
+CORS_ALLOWED_ORIGINS = ['https://vue-shopping-cart.nicholasmoen.com']
 CORS_ALLOW_ALL_ORIGINS = False
